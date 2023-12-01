d4c15b1cf4e184d125d9fc6ffafe77a5c549f3d9
Ananya0214 <147363813+Ananya0214@users.noreply.github.com>
2023-12-01 18:52:29 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const qr=require('qrcode');
 const bodyParser = require('body-parser');
 var QRCode = require('qrcode');
-
 const app = express();
+const port=3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.get('/', (req, res) => {    
-    res.render('index',{src:''})
+    res.render('index',{src:null})
 })
-
-app.post('/', (req, res) => {
-    let data = req.body.data;
-    QRCode.toDataURL(data, function (err, url){
-        res.render('index',{src: url});
-    })
-})
-
-app.listen (process.env.PORT || 5000, function () {
-    console.log('listening on', process.env.PORT || 5000);
-})
+app.post('/',async (req, res) => {
+    try{
+        const url = req.body.data;
+        const qrImage=await qr.toDataURL(url);
+        res.render('index',{src:qrImage});
+    } catch (err){
+        res.render('index',{src:null});
+    }
+    });
+app.listen(port, () =>console.log(`Listening on port ${port}`));
